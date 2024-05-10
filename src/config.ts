a1f16c2f3af1bc2632cd5bc29571669aec08b372
ThomasVon2021 <1767893964@qq.com>
2024-05-10 08:06:15 +0100

    feature: add a dev config.ts

---
+++ src/config.ts
@@ -0,0 +1,22 @@
+/**
+ *
+ * project config settings file
+ *
+ */
+
+/**
+ * 修改下方的IP为你的开发测试机ip，正式环境，此配置无效
+ */
+const dev_device_ip = '110.40.128.155';
+
+const env = process.env.NODE_ENV;
+import project from '../package.json';
+
+const version = project.version;
+const build = project.build;
+
+export default {
+    version:version,
+    build:build,
+    host_ip : env === 'development' ? dev_device_ip : window.location.hostname,
+}
