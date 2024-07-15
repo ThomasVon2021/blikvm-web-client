99365aff5f551ead4d3e2f3639cd9c8925e0c6e0
ThomasVon2021 <1767893964@qq.com>
2024-07-15 19:22:48 +0800

    fix: mv cookies to localstore

--- src/utils/http.js
+++ src/utils/http.js
@@ -7,18 +7,22 @@ const http = axios.create({
     withCredentials: true
 });
 
-
-http.interceptors.response.use(
-    response => {
-        return response;
-    },
-    error => {
-        if (error.response && error.response.status === 401) {
-            console.error('Authentication failed, redirecting to login page...');
-            window.location.href = '/';
-        }
-        return Promise.reject(error);
+http.interceptors.request.use(config => {
+    const token = localStorage.getItem('token'); 
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
     }
-);
+    const username = localStorage.getItem('username');
+    if (username) {
+        config.headers.Username = username;
+    }
+    return config;
+}, error => {
+    if (error.response && error.response.status === 401) {
+        console.error('Authentication failed, redirecting to login page...');
+        window.location.href = '/';
+    }
+    return Promise.reject(error);
+});
 
 export default http;
