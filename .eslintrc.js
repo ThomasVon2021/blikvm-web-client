aac3517744f3c7982accb2c54a54a1921c097666
ThomasVon2021 <1767893964@qq.com>
2024-08-14 17:15:05 +0800

    chore:opt lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,13 @@ module.exports = {
   env: {
     node: true,
   },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ['@babel/preset-env'],
+    },
+  },
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
