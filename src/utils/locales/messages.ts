16d42b3601cfc1104247edd116a63f59d12c1fe1
Zeth Wong <157788067+ZethWong@users.noreply.github.com>
2024-11-17 19:50:18 +0800

    chore: add Malaysia language
    Update the messages.ts for Malaysia language

--- src/utils/locales/messages.ts
+++ src/utils/locales/messages.ts
@@ -2,12 +2,14 @@ import en from './en.json';
 import zh from './zh.json';
 import IT from './IT.json';
 import UA from './UA.json';
+import ms from './ms.json';
 
 const messages = {
     en: en,
     zh: zh,
     IT: IT,
     UA: UA,
+    ms: ms,
   };
   
   export default messages;
